cce0276efc3c58542d7441fb6ffbf3bff874bda0
joaovictorino <jhvictorino@gmail.com>
2022-08-11 12:52:45 -0300

    http test

---
+++ commands.sh
@@ -0,0 +1,17 @@
+# create project
+func init fuctions-samples --javascript
+
+# create http function
+func new --name create-order --template "HTTP trigger" --authlevel "anonymous"
+
+# Azure
+az login
+
+az group create --name rg-sample-function --location eastus
+
+az storage account create --name sasampleazurefunction --location eastus --resource-group rg-sample-function --sku Standard_LRS
+
+az functionapp create --resource-group rg-sample-function --consumption-plan-location eastus --runtime node --runtime-version 16 --functions-version 4 --name create-order-sample --storage-account sasampleazurefunction
+
+# deploy to Azure
+func azure functionapp publish create-order-sample
